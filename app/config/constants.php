e86d6e03a6fdbd8645d6ad2b0e5fb228cbcb7df5
Rain Kohv <rain.kohv@khk.ee>
2021-03-01 16:32:27 +0200

    Määratud raamistiku konstandid ja projekti parameetrid

--- app/config/constants.php
+++ app/config/constants.php
@@ -1,13 +1,13 @@
 <?php
 //project name
-define('PROJECT_NAME', 'pvk_vs20');
+define('PROJECT_NAME', 'blog_vs20');
 //app root dir
 define('APPROOT', dirname(dirname(__FILE__)));
 //url root
 define('URLROOT', 'http://'.$_SERVER['HTTP_HOST'].'/'.PROJECT_NAME);
 
 //db
-define('DB_HOST', 'host');
-define('DB_USER', 'user');
-define('DB_PASS', 'pass');
-define('DB_NAME', 'dbname');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'blog_vs20_2');
