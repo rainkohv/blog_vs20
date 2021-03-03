13e6fcc652bde4721540b614d254477524e49601
Rain Kohv <rain.kohv@khk.ee>
2021-03-03 22:00:19 +0200

    Kontrollitud registreerimisandmete liikumine serverile vormist.

--- app/views/users/register.php
+++ app/views/users/register.php
@@ -8,7 +8,8 @@
                     <p class="mt-2">Please fill the fields below to register</p>
                 </div>
                 <div class="card-body">
-                    <form class="form" role="form">
+                    <form class="form" role="form" method="post" action="<?php echo URLROOT . '/users/register' ?>">
+
                         <div class="form-group">
                             <label for="name">Name<sup>*</sup></label>
                             <input type="text" class="form-control" id="name" name="name" placeholder="Full name">
