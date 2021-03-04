bff62a7203fb6356a8c3f64f8d1bc8c2fcd04fe1
Rain Kohv <rain.kohv@khk.ee>
2021-03-05 00:50:23 +0200

    Loodud üksiku postituse kuvamine.

--- app/models/Post.php
+++ app/models/Post.php
@@ -28,4 +28,13 @@ class Post
         $result = $this->db->getAll();
         return $result;
     }
+
+    public function getPostById($id)
+    {
+        $this->db->query('SELECT * FROM posts WHERE id=:id');
+        $this->db->bind(':id', $id);
+        $post = $this->db->getOne();
+        return $post;
+    }
+
 }
