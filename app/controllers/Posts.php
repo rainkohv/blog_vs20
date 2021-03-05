d5d6f3f38931e1d230fd32a83028285b768a81a9
Rain Kohv <rain.kohv@khk.ee>
2021-03-05 02:25:50 +0200

    Loodud üksiku postituse redigeerimine.

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -1,6 +1,4 @@
 <?php
-
-
 class Posts extends Controller
 {
     /**
@@ -10,7 +8,6 @@ class Posts extends Controller
     {
         $this->postsModel = $this->model('Post');
     }
-
     public function index()
     {
         $posts = $this->postsModel->getPosts();
@@ -19,7 +16,6 @@ class Posts extends Controller
         );
         $this->view('posts/index', $data);
     }
-
     public function show($id)
     {
         $post = $this->postsModel->getPostById($id);
@@ -28,7 +24,6 @@ class Posts extends Controller
         );
         $this->view('posts/show', $data);
     }
-
     public function delete($id)
     {
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
@@ -44,49 +39,6 @@ class Posts extends Controller
             }
         } else {
             redirect('posts');
-        }
-    }
-
-    public function edit($id)
-    {
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
-            $data = array(
-                'id' => $id,
-                'title' => trim($_POST['title']),
-                'content' => trim($_POST['content']),
-                'user_id' => $_SESSION['user_id'],
-                'title_err' => '',
-                'content_err' => ''
-            );
-            if (empty($data['title'])) {
-                $data['title_err'] = 'Please enter title';
-            }
-            if (empty($data['content'])) {
-                $data['content_err'] = 'Please enter content text';
-            }
-            if (empty($data['title_err']) and empty($data['content_err'])) {
-                if ($this->postsModel->editPost($data)) {
-                    message('post_message', 'Post Updated');
-                    redirect('posts');
-                } else {
-                    die('Something went wrong');
-                }
-            } else {
-                $this->view('posts/edit', $data);
-            }
-
-        } else {
-            $post = $this->postsModel->getPostById($id);
-            if ($post->user_id != $_SESSION['user_id']) {
-                redirect('posts');;
-            }
-            $data = array(
-                'id' => $id,
-                'title' => $post->title,
-                'content' => $post->content
-            );
-            $this->view('posts/edit', $data);
         }
     }
 
